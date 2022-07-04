fb11f95207dd861726e1cc7b393f2cdef87c6b28
MariamHossamEldeen <108455053+MariamHossamEldeen@users.noreply.github.com>
2022-07-04 16:13:56 +0200

    First Commit

---
+++ translations.js
@@ -0,0 +1,28 @@
+const translations = {
+    en: {
+    home: "HOME",
+    services: "SERVICES",
+    projects: "PROJECTS",
+    products: "PRODUCTS",
+    blogs: "BLOGS",
+    clients: "CLIENTS",
+    about:"ABOUT US",
+    contact: "CONTACT US",
+    english: "English",
+    arabic: "Arabic",
+},
+ar: {
+    home: "الصفحة الرئيسية",
+    services:"الخدمات",
+    projects: "المشاريع",
+    products: "الأخبار",
+    blogs:  "الأنظمه",
+    clients: "العملاء",
+    contact: "تواصل معنا",
+    about: "من نحن",
+    english: "الانجليزية",
+    arabic: "العربية",
+},
+};
+
+// export default translations;
